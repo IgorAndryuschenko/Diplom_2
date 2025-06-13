214c3cd9a117853b00eaf13bb840694fb3c2922a
Igor Andryuschenko <IngWarYan@yandex.ru>
2025-06-13 18:23:55 +0700

    "Добавлены тесты API"

---
+++ urls.py
@@ -0,0 +1,8 @@
+
+class Url:
+    BASE_URL = 'https://stellarburgers.nomoreparties.site'
+    CREATE_USER = '/api/auth/register'
+    CHANGING_USER = '/api/auth/user'
+    AUTH_USER = '/api/auth/login'
+    CREATE_ORDERS = '/api/orders'
+    GET_INDREDIENTS = '/api/ingredients'
